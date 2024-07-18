4cbdfd57f77e0a664d0db7498f9715df53f0d2da
RodrigoC-Moura <145346876+RodrigoC-Moura@users.noreply.github.com>
2024-07-18 11:50:35 +0000

    Add files via upload

---
+++ LojaP/db/Inserts/insert.sql
@@ -0,0 +1,30 @@
+-- INSERT statements for Categorias table
+INSERT INTO `Categorias` VALUES (1,'Desporto'),(2,'Campismo'),(3,'Natação'),(4,'Ginasio');
+
+-- INSERT statements for Clientes table
+INSERT INTO `Clientes` VALUES (1,'Novo Nome','novo@email.com',987654321),(2,'Cliente 2','cliente2@exemplo.com',1234567890),(3,'Cliente 3','cliente3@exemplo.com',1234567890);
+
+-- INSERT statements for Fabricantes table
+INSERT INTO `Fabricantes` VALUES (1,'Fabricante X'),(2,'Fabricante Y'),(3,'Fabricante Z');
+
+-- INSERT statements for Funcionarios table
+INSERT INTO `Funcionarios` VALUES (1,'Funcionario 1',1,3000),(2,'Funcionario 2',2,2000),(3,'Funcionario 3',3,1500);
+
+-- INSERT statements for ItensPedido table
+INSERT INTO `ItensPedido` VALUES (1,4,1,12),(1,6,1,200),(2,3,2,6),(2,7,1,150),(3,1,1,15),(3,5,1,100),(4,4,1,12),(4,6,1,200),(5,3,1,6),(5,7,1,150),(6,1,1,15),(6,5,1,100),(7,6,1,200),(8,6,1,200),(9,5,1,100);
+
+-- INSERT statements for Pedidos table
+INSERT INTO `Pedidos` VALUES (1,1,1,'2024-06-15',212),(2,2,2,'2024-06-16',162),(3,3,3,'2024-06-14',115),(4,1,1,'2024-06-13',212),(5,2,2,'2024-06-12',156),(6,3,3,'2024-06-11',115),(7,1,1,'2024-07-15',200),(8,1,1,'2024-07-16',100),(9,1,1,'2024-07-16',100);
+
+-- INSERT statements for Posicao table
+INSERT INTO `Posicao` VALUES (1,'Gerente'),(2,'Vendedor'),(3,'Caixa');
+
+-- INSERT statements for ProdutoCategoria table
+INSERT INTO `ProdutoCategoria` VALUES (1,1),(6,1),(2,2),(5,2),(4,3),(7,3),(3,4);
+
+-- INSERT statements for ProdutoMarca table
+INSERT INTO `ProdutoMarca` VALUES (4,1),(6,1),(2,2),(5,2),(1,3),(3,3),(7,3);
+
+-- INSERT statements for Produtos table
+INSERT INTO `Produtos` VALUES (1,'Bola',15,3),(2,'tenda',50,0),(3,'oculos natação',6,10),(4,'dumbbell',12,2),(5,'Produto A',100,0),(6,'Produto B',200,29),(7,'Produto C',150,20);
+
